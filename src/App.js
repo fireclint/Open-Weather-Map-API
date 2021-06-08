bbf3c979567c27192015b893717ad9f6c36ca13c
fireclint <firebriley@yahoo.com>
2021-06-07 20:04:18 -0500

    updated UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import './App.css';
+import './index.css';
 
 function App() {
   const [location, setLocation] = useState('');
   const [weather, setWeather] = useState([]);
 
-
-
   const search = (e) => {
     if (e.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`)
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+      )
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
@@ -19,34 +19,39 @@ function App() {
     }
   };
 
-
-
-
   return (
-    <div className='App'>
-      <div>
-        <input
-          onChange={(e) => setLocation(e.target.value)}
-          value={location}
-          onKeyPress={search}
-          type='text'
-          placeholder='Search City'
-        />
-      </div>
-      {typeof weather.main != 'undefined' ? (
-
-      <div>
-        <h2>{weather.name} {weather.sys.country}</h2>
-        <p>{weather.weather[0].description}</p>
-        <h1>{weather.main.temp}</h1>
+    <div className='app'>
+      <main>
         <div>
-          <p>H {weather.main.temp_max}</p>
-          <p>L {weather.main.temp_min}</p>
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            onKeyPress={search}
+            type='text'
+            placeholder='Search City'
+            className='search'
+          />
         </div>
-      </div>
-              ) : (
-                ''
-              )}
+        {typeof weather.main != 'undefined' ? (
+          <div className='content'>
+            <h2>
+              {weather.name} {weather.sys.country}
+            </h2>
+            <p>
+              <span style={{ textTransform: 'capitalize', fontSize: '24px' }}>
+                {weather.weather[0].main}
+              </span>
+            </p>
+            <h1>{weather.main.temp.toFixed(0)}°F</h1>
+            <div className='hilo'>
+              <p>H: {weather.main.temp_max.toFixed(0)}°</p>
+              <p>L: {weather.main.temp_min.toFixed(0)}°</p>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </main>
     </div>
   );
 }
